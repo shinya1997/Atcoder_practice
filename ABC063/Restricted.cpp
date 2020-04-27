69554654392164b7b4a7591aa6307f290a1e5b72
Shinya <katsuyamashinya@gmail.com>
2020-04-27 22:15:51 +0900

    atcoder

---
+++ ABC063/Restricted.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B;
+    cin >> A >> B;
+    if(A + B >= 10)
+        cout << "error" << endl;
+    else
+    {
+        cout << A + B << endl;
+    }
+    return 0;
+}
---
+++ ABC064/RGB Cards.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int r, g, b;
+    cin >> r >> g >> b;
+    int n = 100 * r + 10 * g + b;
+    if(n % 4 == 0)
+        cout << "YES" << endl;
+    else
+    {
+        cout << "NO" << endl;
+    }
+    return 0;
+}
